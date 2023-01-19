ba721ae78d973fb884336ed013999bb1e941d44b
Tetraliane <57061591+tetraliane@users.noreply.github.com>
2023-01-19 18:52:13 +0900

    docs: ignore code in docs

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,13 @@
 //!
 //! 1. Import `Game` and `Shape`.
 //! 2. Make a shape selector.
-//!    ```
+//!    ```ignore
 //!    fn selector() -> Shape {
 //!        // Return one of the shapes (probably you want to select randomly)
 //!    }
 //!    ```
 //! 3. Create a game.
-//!    ```
+//!    ```ignore
 //!    fn main() {
 //!        // Create a game which has a 10x20 field and provides 3 next tetriminos
 //!        let mut game = Game::new(10, 20, 3, selector);
