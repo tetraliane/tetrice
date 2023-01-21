753974148565813162b3cdeaad294255327204f5
Tetraliane <57061591+tetraliane@users.noreply.github.com>
2023-01-21 17:44:23 +0900

    Created example module (#19)

--- README.md
+++ README.md
@@ -4,18 +4,22 @@ This library provides the core functions of Tetris.
 
 # How to use
 
-1. Import `Game` and `BlockKind`.
+1. Install this crate.
 2. Make a block kind selector.
    ```rust
+   use tetris::BlockKind;
+
    fn selector() -> BlockKind {
        // Return one of the kinds (probably you want to select randomly)
    }
    ```
 3. Create a game.
    ```rust
+   use tetris::Game;
+
    fn main() {
        // Create a game which has a 10x20 field and provides 3 next tetriminos
-       let mut game = Game::new(10, 20, 3, selector);
+       let mut game = Game::new(10, 20, 3, Box::new(selector));
        // Now you can move, rotate, etc. using `game`!
    }
    ```
@@ -23,5 +27,5 @@ This library provides the core functions of Tetris.
 # Documentation
 
 ```shell
-cargo doc
+cargo doc --package tetris
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -2,18 +2,25 @@
 //!
 //! # How to use
 //!
-//! 1. Import `Game` and `BlockKind`.
+//! 1. Install this crate.
 //! 2. Make a block kind selector.
-//!    ```ignore
+//!    ```
+//!    use tetris::BlockKind;
+//!
 //!    fn selector() -> BlockKind {
 //!        // Return one of the kinds (probably you want to select randomly)
+//!        # BlockKind::T
 //!    }
 //!    ```
 //! 3. Create a game.
-//!    ```ignore
+//!    ```
+//!    use tetris::Game;
+//!    # use tetris::BlockKind;
+//!    # fn selector() -> BlockKind { BlockKind::T }
+//!
 //!    fn main() {
 //!        // Create a game which has a 10x20 field and provides 3 next tetriminos
-//!        let mut game = Game::new(10, 20, 3, selector);
+//!        let mut game = Game::new(10, 20, 3, Box::new(selector));
 //!
 //!        // Now you can move, rotate, etc. using `game`!
 //!    }
